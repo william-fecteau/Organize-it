56944e049cf3d5ca0be744520a3b4f0348e202d6
William Fecteau <william-fecteau@hotmail.com>
2022-01-22 09:38:34 -0500

    Added FontAwesome icon support

--- mchacks2022/ClientApp/src/main.js
+++ mchacks2022/ClientApp/src/main.js
@@ -1,19 +1,25 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
 import { createStore } from "vuex";
-import App from './App.vue'
-import './styles/tailwind_base.css'
-import router from './router'
-import state from './store/state'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import App from "./App.vue";
+import "./styles/tailwind_base.css";
+import router from "./router";
+import state from "./store/state";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPlusSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faPlusSquare);
 
 const store = createStore({
-    state() {
-        return state;
-    }
+  state() {
+    return state;
+  },
 });
-
 
 const app = createApp(App);
 app.use(router).use(store).use(ElementPlus);
-app.mount('#app');
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+app.mount("#app");
