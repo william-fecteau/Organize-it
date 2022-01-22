3219fb6e9c8c72acb5af081cb8e8dc7258b320b8
Jordan <itsseraphii@gmail.com>
2022-01-21 22:24:29 -0500

    Added ElementPlus

--- mchacks2022/ClientApp/src/main.js
+++ mchacks2022/ClientApp/src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 import './styles/tailwind_base.css'
 import router from './router'
 import state from './store/state'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const store = createStore({
     state() {
@@ -13,5 +15,5 @@ const store = createStore({
 
 
 const app = createApp(App);
-app.use(router).use(store);
+app.use(router).use(store).use(ElementPlus);
 app.mount('#app');
