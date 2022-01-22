6de99bbd351884dc406763449a6edaf756e7854e
William Fecteau <william-fecteau@hotmail.com>
2022-01-22 09:54:23 -0500

    Changed NavBar to Menu Bar

--- mchacks2022/ClientApp/src/App.vue
+++ mchacks2022/ClientApp/src/App.vue
@@ -1,14 +1,35 @@
 <template>
-  <el-tabs class="ml-4" v-model="activeName" @tab-click="handleClick">
-    <el-tab-pane label="User" name="first">User</el-tab-pane>
-    <el-tab-pane label="Config" name="second">Config</el-tab-pane>
-    <el-tab-pane label="Role" name="third">Role</el-tab-pane>
-    <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
-  </el-tabs>
+  <el-menu
+      mode="horizontal"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+      :router="true"
+
+  >
+    <el-menu-item index="1">
+      <img class="self-center w-auto" style="height: 50px" alt="McHacks logo" src="./assets/mchacks.png">
+    </el-menu-item>
+
+    <el-menu-item index="/agenda">Agenda
+    </el-menu-item>
+
+    <el-sub-menu>
+      <template #title>Classes</template>
+      <el-menu-item index="/classes/glo-1111">GLO-1111</el-menu-item>
+      <el-menu-item index="/classes/mat-1919">MAT-1919</el-menu-item>
+      <el-menu-item index="/classes/ift-3001">IFT-3001</el-menu-item>
+      <el-menu-item index="/classes/new-class">New class
+        <font-awesome-icon class="ml-2" icon="plus-square"/>
+      </el-menu-item>
+    </el-sub-menu>
+
+    <el-menu-item index="deadlines">Deadlines</el-menu-item>
+  </el-menu>
+
 
   <div class="w-full flex justify-center items-center flex-col pt-4">
     <!-- This should be replaced with a global header or something -->
-    <img class="self-center w-auto" alt="McHacks logo" src="./assets/mchacks.png">
     <MessageDisplay class="my-8" msg="Welcome to McHacks 2022 babyy"/>
     <Counter/>
   </div>
@@ -55,7 +76,7 @@ export default {
           .catch(() => {
             console.log("pain");
           });
-    }
+    },
   }
 }
 </script>
