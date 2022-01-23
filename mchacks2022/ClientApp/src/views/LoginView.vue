d747a05702a691f50ac56fa3b0cbe2a925e51bc2
Jordan <itsseraphii@gmail.com>
2022-01-23 09:07:05 -0500

    How to use the app lol

--- mchacks2022/ClientApp/src/views/LoginView.vue
+++ mchacks2022/ClientApp/src/views/LoginView.vue
@@ -1,5 +1,6 @@
 <template>
   <page-template title="Login">
+    <el-alert title="App in construction, the temporary account is user: 'gamer' and password: '1234'" type="warning" />
     <div style="width: 50%" class="mt-20 mx-auto">
       <el-form ref="formRef" label-width="120px">
         <el-form-item label="Username">
@@ -10,16 +11,17 @@
           <el-input class="w-40" v-model="password" placeholder="Your password..."></el-input>
         </el-form-item>
 
-        <div class="text-red-600" v-show="loginError">Invalid username/password combination</div>
+        <div class="text-red-600" v-show="loginError">
+          <h1>Invalid username/password combination</h1>
+          <el-alert title="Go to /test/seed to reset the database if you believe you entered the right credentials" type="alert" />
+        </div>
 
         <el-form-item>
           <el-button type="primary" @click="onSubmit">Login</el-button>
         </el-form-item>
       </el-form>
     </div>
-
   </page-template>
-
 </template>
 
 <script>
